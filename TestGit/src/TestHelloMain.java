e57ed091f7cab3e4937ca775b681f49af865340c
sumal <sumal@192.168.1.14>
2024-04-04 00:23:55 -0700

    added addtional message

--- TestGit/src/TestHelloMain.java
+++ TestGit/src/TestHelloMain.java
@@ -3,7 +3,8 @@ public class TestHelloMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Hello World...");   
+			System.out.println("Hello World...");
+			System.out.println("Git project test");
 	}
 
 }
